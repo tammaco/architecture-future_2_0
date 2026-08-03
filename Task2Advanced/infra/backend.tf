c9bbfcc5457ec6c1f11e4c7171e02a843c54f138
tammaco <shchegolikhina@gmail.com>
2026-08-03 20:26:58 +0300

    fix19

--- Task2Advanced/infra/backend.tf
+++ Task2Advanced/infra/backend.tf
@@ -1,14 +1,11 @@
 terraform {
-  backend "s3" {
-    endpoint   = "https://storage.yandexcloud.net"
-    bucket     = "architecture-future-state-bucket"
-    region     = "us-east-1"
-    key        = "task2/terraform.tfstate"
-    
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
+  backend "http" {
+    address  = "https://storage.yandexcloud.net/architecture-future-state-bucket/task2/terraform.tfstate"
+    update_method = "PUT"
+    lock_address  = "https://storage.yandexcloud.net/architecture-future-state-bucket/task2/terraform.tfstate.lock"
+    unlock_method = "DELETE"
+    lock_method   = "PUT"
+    username      = var.yandex_access_key
+    password      = var.yandex_secret_key
   }
 }
