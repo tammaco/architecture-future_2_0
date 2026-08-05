0aff6157d8490a02b4b6b3704f679cf740138e30
tammaco <shchegolikhina@gmail.com>
2026-08-05 16:53:41 +0300

    4

--- Task2Advanced/infra/variables.tf
+++ Task2Advanced/infra/variables.tf
@@ -1,4 +1,9 @@
-variable "service_account_key_file" {
+# Обязательные переменные (передаются через секреты)
+variable "subnet_id" {
+  type = string
+}
+
+variable "ssh_public_key_path" {
   type = string
 }
 
@@ -8,4 +13,34 @@ variable "yandex_cloud_id" {
 
 variable "yandex_folder_id" {
   type = string
+}
+
+variable "service_account_key_file" {
+  type = string
+}
+
+# Необязательные переменные (со значениями по умолчанию)
+variable "vm_name" {
+  type    = string
+  default = "dev-ubuntu-vm"
+}
+
+variable "cores" {
+  type    = number
+  default = 2
+}
+
+variable "memory" {
+  type    = number
+  default = 4
+}
+
+variable "disk_size" {
+  type    = number
+  default = 10
+}
+
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
 }
