62c0bce526894cbf953b6e05c8ce6b17a803fc8d
tammaco <shchegolikhina@gmail.com>
2026-08-05 16:37:03 +0300

    terraform-test

--- Task2Advanced/infra/backend.tf
+++ Task2Advanced/infra/backend.tf
@@ -1,17 +1,16 @@
 terraform {
   backend "s3" {
-    endpoint   = "https://storage.yandexcloud.net"
-    bucket     = "architecture-future-state-bucket"
-    region     = "us-east-1"
-    key        = "task2/terraform.tfstate"
-    
-    access_key = ""
-    secret_key = ""
-    
+    endpoint                    = "https://storage.yandexcloud.net"
+    bucket                      = "architecture-future-state-bucket"
+    region                      = "us-east-1"
+    key                         = "task2/terraform.tfstate"
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+
+    access_key = ""
+    secret_key = ""
   }
 }
--- Task2Advanced/infra/provider.tf
+++ Task2Advanced/infra/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.75.0"
+      version = ">= 0.110.0"
     }
   }
 }
 
 provider "yandex" {
   zone = var.zone
-  service_account_key_file = var.service_account_key_file
 }
